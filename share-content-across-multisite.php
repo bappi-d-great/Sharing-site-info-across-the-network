b8a149cf1f9d542763e1c833f04fb217d1a5c61e
Ashok (Bappi D Great) <aknath.707@gmail.com>
2014-11-20 10:50:54 +0600

    Initial commit

---
+++ share-content-across-multisite.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Share across network
+Plugin URI: http://premium.wpmudev.org/
+Description: Will shows information about all subsites
+Version: 1.0.1
+Author: Ashok (Incsub)
+Author URI: http://bappi-d-great.com
+License: GNU General Public License (Version 2 - GPLv2)
+Network: true
+*/
+
+if ( ! defined( 'ABSPATH' ) ) exit;
